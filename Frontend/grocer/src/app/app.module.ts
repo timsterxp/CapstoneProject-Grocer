f30d92d12ca224ab6d7ef65a76ec2579ad38b6b1
Kanak Sharma <kanaksharma412@gwu.edu>
2021-04-23 14:28:16 -0400

    EmpLogin Frontend

--- Frontend/grocer/src/app/app.module.ts
+++ Frontend/grocer/src/app/app.module.ts
@@ -4,26 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { AdminPageComponent } from './admin-page/admin-page.component';
-import { EmployeePageComponent } from './employee-page/employee-page.component';
-import { UserPageComponent } from './user-page/user-page.component';
 import { UserSigninComponent } from './user-signin/user-signin.component';
-import { AdminOrEmpSigninComponent } from './admin-or-emp-signin/admin-or-emp-signin.component';
 import { UserHomeComponent } from './user-home/user-home.component';
 import { EmpSigninComponent } from './emp-signin/emp-signin.component';
 import { AdminSigninComponent } from './admin-signin/admin-signin.component';
 import { EmpHomeComponent } from './emp-home/emp-home.component';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    AdminPageComponent,
-    EmployeePageComponent,
-    UserPageComponent,
     UserSigninComponent,
-    AdminOrEmpSigninComponent,
     UserHomeComponent,
     EmpSigninComponent,
     AdminSigninComponent,
@@ -32,7 +29,8 @@ import { AdminHomeComponent } from './admin-home/admin-home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,MatButtonModule,MatInputModule,FormsModule,MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
