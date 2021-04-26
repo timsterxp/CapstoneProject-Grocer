9cb6f21fb577ad190604ebc0c3cc212683f6113a
nap73 <nap73@njit.edu>
2021-04-26 13:45:17 -0400

    user-signup/login/auth

---
+++ Backend/app.js
@@ -0,0 +1,44 @@
+// load all modules 
+let app = require('express')();
+let bodyParser = require('body-parser') ;
+let mongoose = require('mongoose') ;
+let cors = require('cors');
+
+
+var User = require('./router/grocer.router.js') ;
+
+
+
+const mongooseDB = {
+    useNewUrlParser : true ,
+    useUnifiedTopology : true
+}
+
+
+
+// connection to mongodb cloud database online 
+const uri = "mongodb+srv://admin:admin@cluster0.lb81w.mongodb.net/grocer-db?retryWrites=true&w=majority";
+
+
+
+app.use(bodyParser.urlencoded({extended:true})) ;
+app.use(bodyParser.json()) ;
+app.use(cors()) ;
+
+
+mongoose.connect(uri , mongooseDB ) ;
+
+mongoose.connection;
+
+
+app.use("/user" , User) ;
+
+
+app.listen(9090, () => {
+    console.log('Server Running on Port Number : 9090' ) ;
+})
+
+
+
+
+
