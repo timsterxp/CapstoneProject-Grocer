b49b37783dc5c03378d9078a87568115a277a2a3
Tim Nguyen <timmyn97@gmail.com>
2021-04-29 13:51:05 -0700

    Finish merging Lakshmi new stuff

--- Backend/controller/admin.controller.js
+++ Backend/controller/admin.controller.js
@@ -1,11 +1,11 @@
-let AdminModel = require("../model/admin.model.js");
+  let AdminModel = require("../model/admin.model.js");
 
-let getAllAdmins = (req, res) => {
-    AdminModel.find({}, (err, result) => {
-        if (!err) {
-            res.json(result);
-        }
-    })
-}
+  let getAllAdmins = (req, res) => {
+      AdminModel.find({}, (err, result) => {
+          if (!err) {
+              res.json(result);
+          }
+      })
+  }
 
-module.exports = { getAllAdmins };
+  module.exports = { getAllAdmins };
--- Frontend/grocer/src/app/Admin/view-request/view-request.component.ts
+++ Frontend/grocer/src/app/Admin/view-request/view-request.component.ts
@@ -1,4 +1,7 @@
+  
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ViewRequestService } from './view-request.service';
 
 @Component({
   selector: 'app-view-request',
@@ -8,17 +11,42 @@ import { Component, OnInit } from '@angular/core';
 export class ViewRequestComponent implements OnInit {
  /* requests?:Array<Request>
     constructor(public requestSer:RequestService){}
-
     ngOnInit():void{
       this.requestSer.retrieveAllRequests().subscribe(result=>this.requests=result);
     }
  //This code is all dependent on what the employee person has set up in regards to requests.
  //Will be edited later   
  */
-  constructor() { }
+  constructor(private viewService: ViewRequestService, public router:Router) { }
+  requests:any;
+  Msg:any;
+  Option:String;
 
   ngOnInit(): void {
+    this.getRequests();
   }
+
+  Update(status:any){
+    console.log(status)
+    this.viewService.updateRequest(status).subscribe((result:String)=>{
+      this.Msg = result;
+      this.getRequests();
+      
+    });
+
+    
+  }
+
+  getRequests(){
+    this.viewService.retrieveAllRequest().subscribe((result)=>{
+      this.requests=result
+    })
+  }
+
+  back(){
+    this.router.navigate(["AdminHome"]);
+  }
+
 
   displayedColumns=['Employee Id','Request'];
 
